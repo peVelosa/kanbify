a55409f419ab7bba1db74a9a517023802794301e
Pedro Enrique Pereira Velosa <m_pedro.velosa@fiap.com.br>
2024-03-01 17:45:30 -0300

    suspensa boundary

--- src/app/auth/login/features/form/login-form.tsx
+++ src/app/auth/login/features/form/login-form.tsx
@@ -18,7 +18,7 @@ import { TLoginSchema } from "@/app/actions/login/type";
 import { login } from "@/app/actions/login";
 import FormError from "@/app/auth/_components/form/form-error";
 import FormWarning from "@/app/auth/_components/form/form-warning";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import ResendVerificationEmail from "./resend-verification-email";
 import { useSearchParams } from "next/navigation";
 
@@ -41,44 +41,49 @@ export default function LoginForm() {
   };
 
   return (
+    <>
+    <Suspense fallback={<h1>loading...</h1>}>
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="john.doe@email.com" {...field} />
-              </FormControl>
-              <FormMessage />
-              <ResendVerificationEmail
-                warning={message?.warning}
-                email={field.value}
-              />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <PasswordInput field={field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormError error={message?.error} />
-        <FormWarning warning={message?.warning} />
-        <Button type="submit" className="w-full">
-          Login
-        </Button>
-      </form>
-    </Form>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="john.doe@email.com" {...field} />
+                </FormControl>
+                <FormMessage />
+                <ResendVerificationEmail
+                  warning={message?.warning}
+                  email={field.value}
+                />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <PasswordInput field={field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormError error={message?.error} />
+          <FormWarning warning={message?.warning} />
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </form>
+      </Form>
+    </Suspense>
+  
+    </>
   );
 }
