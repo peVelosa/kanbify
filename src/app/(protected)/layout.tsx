320c07b548376f5b81e42594825ae470a2e36d77
Pedro Velosa <pedro.velosa@fiap.com.br>
2024-04-17 01:41:07 -0300

    metadata

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,5 +1,25 @@
+import { Metadata, ResolvingMetadata } from "next";
 import Navbar from "./_components/navbar";
+import { auth } from "@/auth";
 
+export async function generateMetadata(
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const user = await auth();
+
+  return {
+    title: `Kanbify | ${user?.user?.name}`,
+    applicationName: "Kanbify",
+    authors: {
+      name: "Kanbify",
+      url: "https://kanbify.vercel.app",
+    },
+    category: "Productivity",
+    keywords: ["kanban", "tasks", "productivity", "teams"],
+    creator: "Pedro Velosa",
+    publisher: "Pedro Velosa",
+  };
+}
 export default function ProtectedLayout({
   children,
 }: Readonly<{
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,17 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kanbify",
+  description:
+    "Kanbify is a simple kanban board app where you can manage your team's tasks.",
+  applicationName: "Kanbify",
+  authors: {
+    name: "Kanbify",
+    url: "https://kanbify.vercel.app",
+  },
+  category: "Productivity",
+  keywords: ["kanban", "tasks", "productivity", "teams"],
+  creator: "Pedro Velosa",
+  publisher: "Pedro Velosa",
 };
 
 export default function RootLayout({
