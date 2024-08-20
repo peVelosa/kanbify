68960b520e75af80eb7cf57f6b70bdeea7921d85
Pedro Velosa <pedro.velosa@fiap.com.br>
2024-08-20 16:08:08 -0300

    fix: ajuste env

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,6 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./server/auth";
-import {
-  publicRoutes,
-  authRoutes,
-  apiAuthPrefix,
-  DEFAULT_REDIRECT,
-  validationRoutes,
-} from "@/routes";
+import { publicRoutes, DEFAULT_REDIRECT } from "@/routes";
 
 const { auth } = NextAuth(authConfig);
 
@@ -14,27 +8,6 @@ export default auth((req) => {
   const { nextUrl } = req;
 
   const isAuth = !!req.auth;
-
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-  const isAuthApiPrefix = apiAuthPrefix.some((prefix) => nextUrl.pathname.startsWith(prefix));
-  const isVerifyRoute = validationRoutes.includes(nextUrl.pathname);
-
-  if (isPublicRoute || isAuthApiPrefix || isVerifyRoute) return;
-
-  if (isAuthRoute && !isAuth) return;
-
-  if (isAuthRoute && isAuth) return Response.redirect(new URL(DEFAULT_REDIRECT, nextUrl));
-
-  if (!isAuth && !isPublicRoute) {
-    let callbackUrl = nextUrl.pathname;
-    if (nextUrl.search) {
-      callbackUrl += nextUrl.search;
-    }
-    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-
-    return Response.redirect(new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl));
-  }
 
   return;
 });
--- src/routes.ts
+++ src/routes.ts
@@ -3,23 +3,4 @@
  */
 export const publicRoutes = ["/"];
 
-/**
- * Contains all routes used for authentication.
- */
-export const authRoutes = ["/auth/login", "/auth/register"];
-
-/**
- * Contains all API routes used for authentication.
- */
-export const apiAuthPrefix = ["/api/auth", "/api/trpc/auth.login", "/api/trpc/auth.register"];
-
-/**
- * Contains all routes used for validation
- * @exemple verify/reset-password.
- */
-export const validationRoutes = ["/verify"];
-
-/*
- * Default redirect route after login.
- */
 export const DEFAULT_REDIRECT = "/dashboard";
